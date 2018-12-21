601044e5656eaf71fbba4aa47d76b84b8840ac90
Ray Lee <rhlee@berkeley.edu>
2018-12-21 00:45:06 -0800

    Add runBatch script.

---
+++ src/helpers/forEach.js
@@ -0,0 +1,53 @@
+import yargs from 'yargs';
+import get from 'lodash/get';
+import log from './log';
+
+const {
+  pageSize,
+  startPage,
+} = yargs.argv;
+
+const pgSz = (pageSize && pageSize > 0) ? pageSize : 100;
+
+const processPage = (cspace, service, params, callback) => cspace.read(service, { params })
+  .then((result) => {
+    const itemsInPage = parseInt(get(result, ['data', 'ns2:abstract-common-list', 'itemsInPage']), 10);
+
+    if (itemsInPage > 0) {
+      let items = get(result, ['data', 'ns2:abstract-common-list', 'list-item']);
+
+      if (!Array.isArray(items)) {
+        items = [items];
+      }
+
+      items.reduce((promise, item) => promise
+        .then(Promise.resolve(callback(item))), Promise.resolve());
+    }
+
+    return itemsInPage;
+  });
+
+export default async (cspace, service, params, callback) => {
+  let pgNum = startPage || 0;
+  let itemsInPage;
+
+  do {
+    const pageParams = Object.assign({}, params, {
+      pgNum,
+      pgSz,
+      sortBy: 'collectionspace_core:createdAt',
+    });
+
+    log(`processing page ${pgNum} with size ${pgSz}`);
+
+    try {
+      itemsInPage = await processPage(cspace, service, pageParams, callback);
+    } catch (error) {
+      log(error);
+
+      break;
+    }
+
+    pgNum += 1;
+  } while (itemsInPage === pgSz);
+};
---
+++ src/scripts/generic/runBatch.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-console */
+
+import cspaceAPI from 'cspace-api';
+import yargs from 'yargs';
+import get from 'lodash/get';
+import log from '../../helpers/log';
+import getConnectionInfo from '../../helpers/getConnectionInfo';
+import forEach from '../../helpers/forEach';
+
+const cspace = cspaceAPI(getConnectionInfo());
+
+const {
+  batchCsid,
+  docType,
+  service,
+  params: paramsJson,
+} = yargs.argv;
+
+const params = paramsJson ? JSON.parse(paramsJson) : undefined;
+
+log('Using search params:', params);
+
+forEach(cspace, service, params, (item) => {
+  const config = {
+    data: {
+      'ns2:invocationContext': {
+        '@xmlns:ns2': 'http://collectionspace.org/services/common/invocable',
+        docType,
+        mode: 'single',
+        singleCSID: item.csid,
+      },
+    },
+  };
+
+  return cspace.create(`batch/${batchCsid}`, config)
+    .then((response) => {
+      const numAffected = parseInt(get(response, ['data', 'ns2:invocationResults', 'numAffected']), 10);
+
+      if (numAffected > 0) {
+        log(`updated ${item.csid}`);
+      }
+    })
+    .catch((error) => {
+      log(error);
+    });
+});
